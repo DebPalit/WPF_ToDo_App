7cf93b79403fbfa3714f31c14d7bee21b0bbe630
DebPalit <dpalit.official@gmail.com>
2026-03-08 12:15:04 +0530

    added both create and remove button functionalities with checkboxes

--- WPF_ToDo_App/MainWindow.xaml.cs
+++ WPF_ToDo_App/MainWindow.xaml.cs
@@ -27,20 +27,55 @@ namespace WPF_ToDo_App
 
             if (!string.IsNullOrEmpty(taskInputText))
             {
-                TextBlock newTask = new TextBlock
-                {
-                    Text = taskInputText,
-                    Margin = new Thickness(5),
-                    Foreground = new SolidColorBrush(Colors.White)
-                };
+                string taskInputText_timestamp = $"[{DateTime.Now}]: {taskInputText}";
+                //TextBlock newTask = new TextBlock
+                //{
+                //    Text = taskInputText,
+                //    Margin = new Thickness(5),
+                //    Foreground = new SolidColorBrush(Colors.White)
+                //};
 
-                taskShow.Children.Add(newTask);
+                //taskShow.Children.Add(newTask);
+                CreateDynamicCheckBox(taskInputText_timestamp);
                 taskInput.Clear();
             }
             else
             {
                 MessageBox.Show("Please enter a task before creating a To-Do item.", "Input Required", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
+        }
+        private void removeTodo_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveCheckedTasks(taskShow);
+        }
+
+        private void CreateDynamicCheckBox(string taskText)
+        {
+            CheckBox newCheckBox = new CheckBox
+            {
+                Content = taskText,
+                Margin = new Thickness(5),
+                Foreground = new SolidColorBrush(Colors.DarkBlue),
+                FontWeight = FontWeights.Bold,
+                FontSize = 15
+            };
+            taskShow.Children.Add(newCheckBox);
+        }
+        private void RemoveCheckedTasks(StackPanel stackPanel)
+        {
+            List<CheckBox> toRemove = stackPanel.Children
+                            .OfType<CheckBox>()
+                            .Where(cb => cb.IsChecked == true)
+                            .ToList();
+
+            if (!toRemove.Any())
+            {
+                MessageBox.Show("Please select a task to remove.", "No Task Selected", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            foreach (CheckBox cb in toRemove)
+                stackPanel.Children.Remove(cb);
         }
     }
 }
